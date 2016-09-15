c1daccd95f54a291a9016dca0256ff3719856fb7
Jose Vidal <javidgon@gmail.com>
2016-09-15 21:51:50 +0200

    Change http by https

--- statics/js/index.js
+++ statics/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.getJSON('http://freegeoip.net/json/?callback=?', function(data) {
+  $.getJSON('https://freegeoip.net/json/?callback=?', function(data) {
     $('#ip').text(data.ip);
     //console.log(JSON.stringify(data, null, 2));
   })
